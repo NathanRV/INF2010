9ba6c0286afc76c257ca428ac768ba66b630f69a
Nathan Ramsay-Vejlens <49606381+NathanRV@users.noreply.github.com>
2020-01-24 11:54:25 -0500

    resolution de pointConstructor et methodsDontChangeInternal

--- TP1/labo/labo/src/tp1/Point2d.java
+++ TP1/labo/labo/src/tp1/Point2d.java
@@ -13,7 +13,7 @@ public class Point2d extends AbstractPoint {
 
     // TODO creer un point a partir d'un vecteur de donnees
     public Point2d(Double[] vector) {
-        super(vector);
+        super(vector.clone());
     }
 
     public Double X() { return vector[X];}
@@ -22,50 +22,57 @@ public class Point2d extends AbstractPoint {
     // TODO prendre un vecteur de donnees et appliquer la translation.
     @Override
     public Point2d translate(Double[] translateVector) {
-        this.vector=PointOperator.translate(this.vector,translateVector);
-        return this;
+        Point2d point2d=new Point2d(this.vector);
+        point2d.vector=PointOperator.translate(this.vector,translateVector);
+        return point2d;
     }
 
     // TODO prendre un point et appliquer la translation.
     public Point2d translate(Point2d translateVector) {
-        this.vector=PointOperator.translate(this.vector,translateVector.vector);
-        return this;
+        Point2d point2d=new Point2d(this.vector);
+        point2d.vector=PointOperator.translate(this.vector,translateVector.vector);
+        return point2d;
     }
 
     // TODO prendre un vecteur de donnees et appliquer la rotation.
     @Override
     public Point2d rotate(Double[][] rotationMatrix) {
-        this.vector=PointOperator.rotate(this.vector,rotationMatrix);
-        return this;
+        Point2d point2d=new Point2d(this.vector);
+        point2d.vector=PointOperator.rotate(this.vector,rotationMatrix);
+        return point2d;
     }
 
     // TODO prendre un angle de rotation, creer une matrice et appliquer la rotation.
     public Point2d rotate(Double angle) {
         Double[][] rotationMatrix={{Math.cos(angle),-Math.sin(angle)},
                                     {Math.sin(angle),Math.cos(angle)}};
-        this.vector=PointOperator.rotate(this.vector,rotationMatrix);
-        return this;
+        Point2d point2d=new Point2d(this.vector);
+        point2d.vector=PointOperator.rotate(this.vector,rotationMatrix);
+        return point2d;
     }
 
     // TODO prendre un facteur de division et l'appliquer.
     @Override
     public Point2d divide(Double divider) {
-        this.vector=PointOperator.divide(this.vector,divider);
-        return this;
+        Point2d point2d=new Point2d(this.vector);
+        point2d.vector=PointOperator.divide(this.vector,divider);
+        return point2d;
     }
 
     // TODO prendre un facteur de multiplication et l'appliquer.
     @Override
     public Point2d multiply(Double multiplier) {
-        this.vector=PointOperator.multiply(this.vector,multiplier);
-        return this;
+        Point2d point2d=new Point2d(this.vector);
+        point2d.vector=PointOperator.multiply(this.vector,multiplier);
+        return point2d;
     }
 
     // TODO prendre un facteur d'addition et l'appliquer.
     @Override
     public Point2d add(Double adder) {
-        this.vector=PointOperator.add(this.vector,adder);
-        return this;
+        Point2d point2d=new Point2d(this.vector);
+        point2d.vector=PointOperator.add(this.vector,adder);
+        return point2d;
     }
 
     // TODO creer un nouveau point.
