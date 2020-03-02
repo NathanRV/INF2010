901f935275fba0155f3767de63add3c5a21a1162
Ccassyie <55399587+Ccassyie@users.noreply.github.com>
2020-03-02 00:05:08 -0500

    Final work

--- TP3/tp3_BinaryTrees/src/tp3/AvlTree.java
+++ TP3/tp3_BinaryTrees/src/tp3/AvlTree.java
@@ -10,10 +10,12 @@ public class AvlTree<ValueType extends Comparable<? super ValueType> > {
 
     private BinaryNode<ValueType> root;
 
-    public AvlTree() { }
+    public AvlTree() {
+    }
 
     /**
      * Adds value to the tree and keeps it as a balanced AVL Tree
+     *
      * @param value value to add to the tree
      */
     public void insert(ValueType value) {
@@ -26,14 +28,16 @@ public class AvlTree<ValueType extends Comparable<? super ValueType> > {
 
     /**
      * Removes value from the tree and keeps it as a balanced AVL Tree
+     *
      * @param value value to add to the tree
      */
-    public void remove(ValueType value){
+    public void remove(ValueType value) {
         remove(value, root);
     }
 
     /**
      * Verifies if the tree contains value
+     *
      * @param value value to verify
      * @return if value already exists in the tree
      */
@@ -43,6 +47,7 @@ public class AvlTree<ValueType extends Comparable<? super ValueType> > {
 
     /**
      * Returns the max level contained in our root tree
+     *
      * @return Max level contained in our root tree
      */
     public int getHeight() {
@@ -51,6 +56,7 @@ public class AvlTree<ValueType extends Comparable<? super ValueType> > {
 
     /**
      * Returns the minimal value contained in our root tree
+     *
      * @return Minimal value contained in our root tree
      */
     public ValueType findMin() {
@@ -61,6 +67,7 @@ public class AvlTree<ValueType extends Comparable<? super ValueType> > {
 
     /**
      * Returns all values contained in the root tree in ascending order
+     *
      * @return Values contained in the root tree in ascending order
      */
     public List<ValueType> infixOrder() {
@@ -71,14 +78,15 @@ public class AvlTree<ValueType extends Comparable<? super ValueType> > {
 
     /**
      * Returns all values contained in the root tree in level order from top to bottom
+     *
      * @return Values contained in the root tree in level order from top to bottom
      */
-    public List<ValueType> levelOrder(){
+    public List<ValueType> levelOrder() {
         List<ValueType> items = new LinkedList<ValueType>();
 
         ArrayDeque<BinaryNode<ValueType>> nodesToCheck = new ArrayDeque<BinaryNode<ValueType>>();
 
-        if (root != null){
+        if (root != null) {
             nodesToCheck.push(root);
             levelOrder(nodesToCheck, items);
         }
@@ -86,51 +94,50 @@ public class AvlTree<ValueType extends Comparable<? super ValueType> > {
         return items;
     }
 
-    /** TODO O( log n )
+    /**
+     * TODO O( log n )
      * Adds value to the tree and keeps it as a balanced AVL Tree
      * Should call balance only if insertion succeeds
      * AVL Trees do not contain duplicates
      *
-     * @param value value to add to the tree
+     * @param value       value to add to the tree
      * @param currentNode Node currently being accessed in this recursive method
      * @return if parent node should balance
      */
-    private boolean insert (ValueType value, BinaryNode<ValueType> currentNode){
+    private boolean insert(ValueType value, BinaryNode<ValueType> currentNode) {
 
-          if(value.compareTo(currentNode.value)>0) {
-            if(currentNode.right==null){
-                currentNode.right= new BinaryNode<ValueType>(value,currentNode);
+        if (value.compareTo(currentNode.value) > 0) {
+            if (currentNode.right == null) {
+                currentNode.right = new BinaryNode<ValueType>(value, currentNode);
                 balance(currentNode); //TODO
-            }
-            else {
+            } else {
                 insert(value, currentNode.right);
             }
-        }
-        else if(value.compareTo(currentNode.value)<0){
-            if(currentNode.left==null){
-                currentNode.left= new BinaryNode<ValueType>(value,currentNode);
+        } else if (value.compareTo(currentNode.value) < 0) {
+            if (currentNode.left == null) {
+                currentNode.left = new BinaryNode<ValueType>(value, currentNode);
                 balance(currentNode); //TODO
-            }
-            else {
+            } else {
                 insert(value, currentNode.left);
             }
-        }
-        else{
-            ;//if double, not insert
+        } else {
+            ;
         }
         return true;
     }
 
-    /** TODO O ( log n )
+    /**
+     * TODO O ( log n )
      * Removes value from the tree and keeps it as a balanced AVL Tree
      * Should call balance only if removal succeeds
-     * @param value value to remove from the tree
+     *
+     * @param value       value to remove from the tree
      * @param currentNode Node currently being accessed in this recursive method
      * @return if parent node should balance
      */
     private boolean remove(ValueType value, BinaryNode<ValueType> currentNode) {
-        //Pre-condition
-       while (currentNode!=null) {
+
+        if (currentNode != null) {
             if (value.compareTo(currentNode.value) > 0) {
                 return remove(value, currentNode.right);
 
@@ -158,178 +165,195 @@ public class AvlTree<ValueType extends Comparable<? super ValueType> > {
         return false;
     }
 
-    /** TODO O( n )
+    /**
+     * TODO O( n )
      * Balances the subTree
+     *
      * @param subTree SubTree currently being accessed to verify if it respects the AVL balancing rule
      */
     private void balance(BinaryNode<ValueType> subTree) {
-        //pre-condition
-        if(subTree==null) return;
 
-        //4 rules of balance
-        if(getLevelCount(subTree.left)-getLevelCount(subTree.right)>1){
-            if(getLevelCount(subTree.left.left)-getLevelCount(subTree.left.right)<0){
+        if (subTree == null) return;
+
+        if (getLevelCount(subTree.left) - getLevelCount(subTree.right) > 1) {
+            if (getLevelCount(subTree.left.left) - getLevelCount(subTree.left.right) < 0) {
                 doubleRotateOnLeftChild(subTree);
-            }
-            else{
+            } else {
                 rotateLeft(subTree);
             }
 
-        }
-        else if(getLevelCount(subTree.left)-getLevelCount(subTree.right)<- 1){
-            if(getLevelCount(subTree.right.left)-getLevelCount(subTree.right.right)>0){
+        } else if (getLevelCount(subTree.left) - getLevelCount(subTree.right) < -1) {
+            if (getLevelCount(subTree.right.left) - getLevelCount(subTree.right.right) > 0) {
                 doubleRotateOnRightChild(subTree);
-            }
-            else{
+            } else {
                 rotateRight(subTree);
             }
         }
-        if (subTree!=root)
+        if (subTree != root)
             balance(subTree.parent);
 
     }
 
-    /** TODO O( 1 )
+    /**
+     * TODO O( 1 )
      * Single rotation to the left child, AVR Algorithm
+     *
      * @param node1 Node to become child of its left child
      */
-    private void rotateLeft(BinaryNode<ValueType> node1){
+    private void rotateLeft(BinaryNode<ValueType> node1) {
         BinaryNode<ValueType> leftChild = node1.left;
-        BinaryNode<ValueType> parentNode=node1.parent;
-        leftChild.parent= parentNode;
+        BinaryNode<ValueType> parentNode = node1.parent;
+        leftChild.parent = parentNode;
 
-        if (parentNode!=null) {
-            if (parentNode.value.compareTo(leftChild.value) < 0)
+        if (parentNode != null) {
+            if (parentNode.value.compareTo(leftChild.value) < 0) {
                 parentNode.right = leftChild;
-            else
+            } else {
                 parentNode.left = leftChild;
-        }
-        else
+            }
+        } else {
             root = leftChild;
+        }
         node1.left = leftChild.right;
-        if (leftChild.right!=null)
-            leftChild.right.parent=node1;
+        if (leftChild.right != null) {
+            leftChild.right.parent = node1;
+        }
         leftChild.right = node1;
     }
 
-    /** TODO O( 1 )
+    /**
+     * TODO O( 1 )
      * Single rotation to the right, AVR Algorithm
+     *
      * @param node1 Node to become child of its right child
      */
-    private void rotateRight(BinaryNode<ValueType> node1){
+    private void rotateRight(BinaryNode<ValueType> node1) {
 
         BinaryNode<ValueType> rightChild = node1.right;
-        rightChild.parent= node1.parent;
+        rightChild.parent = node1.parent;
         node1.parent = rightChild;
-        if (rightChild.parent!=null) {
-            if (rightChild.parent.value.compareTo(rightChild.value) < 0)
+
+        if (rightChild.parent != null) {
+            if (rightChild.parent.value.compareTo(rightChild.value) < 0) {
                 rightChild.parent.right = rightChild;
-            else
+            } else {
                 rightChild.parent.left = rightChild;
-        }
-        else
+            }
+        } else {
             root = rightChild;
+        }
         node1.right = rightChild.left;
-        if (rightChild.left!=null)
-            rightChild.left.parent=node1;
+        if (rightChild.left != null) {
+            rightChild.left.parent = node1;
+        }
         rightChild.left = node1;
     }
 
-    /** TODO O( 1 )
+    /**
+     * TODO O( 1 )
      * Double rotation on left child, AVR Algorithm
+     *
      * @param node1 Node to become child of the right child of its left child
      */
-    private void doubleRotateOnLeftChild(BinaryNode<ValueType> node1){
+    private void doubleRotateOnLeftChild(BinaryNode<ValueType> node1) {
         rotateRight(node1.left);
         rotateLeft(node1);
     }
 
-    /** TODO O( 1 )
+    /**
+     * TODO O( 1 )
      * Double rotation on right child, AVR Algorithm
+     *
      * @param node1 Node to become child of the left child of its right child
      */
-    private void doubleRotateOnRightChild(BinaryNode<ValueType> node1){
+    private void doubleRotateOnRightChild(BinaryNode<ValueType> node1) {
         rotateLeft(node1.right);
         rotateRight(node1);
     }
 
-    /** TODO O( log n ) //not sure if O(log n) or O(n)
+    /**
+     * TODO O( log n ) //not sure if O(log n) or O(n)
      * Verifies if the root tree contains value
-     * @param value value to verify
+     *
+     * @param value       value to verify
      * @param currentNode Node currently being accessed in this recursive method
      * @return if value already exists in the root tree
      */
-    private boolean contains(ValueType value, BinaryNode<ValueType> currentNode){
-        if(currentNode==null){
+    private boolean contains(ValueType value, BinaryNode<ValueType> currentNode) {
+        if (currentNode == null) {
             return false;
         }
 
-        if(value.compareTo(currentNode.value)>0){
-            return contains(value,currentNode.right);
-        }
-        else if(value.compareTo(currentNode.value)<0){
-            return contains(value,currentNode.left);
-        }
-        else{  //if equal
+        if (value.compareTo(currentNode.value) > 0) {
+            return contains(value, currentNode.right);
+        } else if (value.compareTo(currentNode.value) < 0) {
+            return contains(value, currentNode.left);
+        } else {
             return true;
         }
     }
 
-    /** TODO O( n )
+    /**
+     * TODO O( n )
      * Returns the number of level contained in subTree including subTree node level
+     *
      * @return Number of level contained in subTree including subTree node level
      */
-    private int getLevelCount(BinaryNode<ValueType> subTree){
-        if(subTree==null){
+    private int getLevelCount(BinaryNode<ValueType> subTree) {
+        if (subTree == null) {
             return 0;
-        }
-        else if(subTree.right != null || subTree.left != null){
-            return (Math.max(getLevelCount(subTree.right),getLevelCount(subTree.left))+1);
-        }
-        else {
+        } else if (subTree.right != null || subTree.left != null) {
+            return (Math.max(getLevelCount(subTree.right), getLevelCount(subTree.left)) + 1);
+        } else {
             return 1;
         }
     }
 
-    /** TODO O( log n )
+    /**
+     * TODO O( log n )
      * Returns the node which has the minimal value contained in our root tree
+     *
      * @return Node which has the minimal value contained in our root tree
      */
     private BinaryNode<ValueType> findMin(BinaryNode<ValueType> currentNode) {
-        if(currentNode == null) return null;
-        if(currentNode.left != null){
+        if (currentNode == null) return null;
+        if (currentNode.left != null) {
             return findMin(currentNode.left);
         }
         return currentNode;
     }
 
-    /** TODO O( n )
+    /**
+     * TODO O( n )
      * Builds items which should contain all values within the root tree in ascending order
+     *
      * @param currentNode Node currently being accessed in this recursive method
-     * @param items List being modified to contain all values in the root tree in ascending order
+     * @param items       List being modified to contain all values in the root tree in ascending order
      */
-    private void infixOrder(BinaryNode<ValueType> currentNode, List<ValueType> items){
-        //check if smaller values
-        if(currentNode.left != null && !items.contains(currentNode.left.value)){
+    private void infixOrder(BinaryNode<ValueType> currentNode, List<ValueType> items) {
+
+        if (currentNode.left != null && !items.contains(currentNode.left.value)) {
             infixOrder(currentNode.left, items);
         }
-        //add current value
-        if(!items.contains(currentNode.value))
+
+        if (!items.contains(currentNode.value))
             items.add(currentNode.value);
-        //check if bigger value
-        if(currentNode.right!=null && !items.contains(currentNode.right.value)){
-            infixOrder(currentNode.right,items);
+
+        if (currentNode.right != null && !items.contains(currentNode.right.value)) {
+            infixOrder(currentNode.right, items);
         }
     }
 
-    /** TODO O( n )
+    /**
+     * TODO O( n )
      * Builds items which should contain all values within the root tree in level order from top to bottom
+     *
      * @param nodesToCheck Queue for non-recursive algorithm
-     * @param items List being modified to contain all values in the root tree in level order from top to bottom
+     * @param items        List being modified to contain all values in the root tree in level order from top to bottom
      */
     private void levelOrder(ArrayDeque<BinaryNode<ValueType>> nodesToCheck, List<ValueType> items) {
-               BinaryNode<ValueType> actualNode;
-        while(!nodesToCheck.isEmpty()){
+        BinaryNode<ValueType> actualNode;
+        while (!nodesToCheck.isEmpty()) {
             actualNode = nodesToCheck.pollFirst();
             if (actualNode.left != null)
                 nodesToCheck.addLast(actualNode.left);
@@ -340,7 +364,7 @@ public class AvlTree<ValueType extends Comparable<? super ValueType> > {
 
         }
     }
-    
+
     static class BinaryNode<ValueType> {
         ValueType value;
 
@@ -349,8 +373,7 @@ public class AvlTree<ValueType extends Comparable<? super ValueType> > {
         BinaryNode<ValueType> left = null; // Pointer to the node on the left which should contain a value below this.value
         BinaryNode<ValueType> right = null; // Pointer to the node on the right which should contain a value above this.value
 
-        BinaryNode(ValueType value, BinaryNode<ValueType> parent)
-        {
+        BinaryNode(ValueType value, BinaryNode<ValueType> parent) {
             this.value = value;
             this.parent = parent;
         }
